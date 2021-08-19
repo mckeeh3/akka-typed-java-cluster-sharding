6ed1064bed4a3dc8366246306cb7d786ea55cba1
Hugh McKee <mckeeh3@gmail.com>
2021-08-19 14:57:22 -0400

    reformat only

--- src/main/resources/viewer.js
+++ src/main/resources/viewer.js
@@ -6,28 +6,28 @@ function sendWebSocketRequest(request) {
     webSocket.send(request);
   } else {
     webSocket = new WebSocket('ws://' + location.host + '/viewer-entities');
-    update({ 'serverActivities': [], 'tree': { 'name': 'cluster', 'type': 'cluster' }});
+    update({ serverActivities: [], tree: { name: 'cluster', type: 'cluster' } });
 
-    webSocket.onopen = function(event) {
+    webSocket.onopen = function (event) {
       console.log('WebSocket connected', event);
       webSocket.send(request);
-    }
+    };
 
-    webSocket.onmessage = function(event) {
+    webSocket.onmessage = function (event) {
       console.log(event);
       const root = JSON.parse(event.data);
       if (svgHasFocus) {
         update(root);
       }
-    }
+    };
 
-    webSocket.onerror = function(error) {
+    webSocket.onerror = function (error) {
       console.error('WebSocket error', error);
-    }
+    };
 
-    webSocket.onclose = function(event) {
+    webSocket.onclose = function (event) {
       console.log('WebSocket close', event);
-    }
+    };
   }
 }
 
@@ -45,42 +45,16 @@ const widthCount = grid * 4;
 
 const messageCountLast = { count: 0, time: new Date() };
 
-const svg = d3.select('svg')
-  .style('width', width)
-  .style('height', height)
-  .style('padding', '0px')
-  .style('box-sizing', 'border-box')
-  .style('font', 'sans-serif');
+const svg = d3.select('svg').style('width', width).style('height', height).style('padding', '0px').style('box-sizing', 'border-box').style('font', 'sans-serif');
 
-svg.append('rect')
-    .attr('width', '100%')
-    .attr('height', '100%')
-    .attr('fill', '#001017');
+svg.append('rect').attr('width', '100%').attr('height', '100%').attr('fill', '#001017');
 
-const g = svg.append('g')
-  .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-
-const gMembers = g.append('g')
-  .attr('class', 'members')
-
-const gLink = g.append('g')
-  .attr('class', 'links')
-  .attr('fill', 'none')
-  .attr('stroke', '#555')
-  .attr('stroke-opacity', '0.4')
-  .attr('stroke-width', 1.5);
-
-const gHttpServerLink = g.append('g')
-  .attr('class', 'http-server-link')
-  .attr('stroke-opacity', '0.4');
-
-const gNode = g.append('g')
-  .attr('class', 'nodes')
-  .attr('stroke-linejoin', 'round')
-  .attr('stroke-width', 3);
-
-const gStatistics = g.append('g')
-  .attr('class', 'statistics')
+const g = svg.append('g').attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+const gMembers = g.append('g').attr('class', 'members');
+const gLink = g.append('g').attr('class', 'links').attr('fill', 'none').attr('stroke', '#555').attr('stroke-opacity', '0.4').attr('stroke-width', 1.5);
+const gHttpServerLink = g.append('g').attr('class', 'http-server-link').attr('stroke-opacity', '0.4');
+const gNode = g.append('g').attr('class', 'nodes').attr('stroke-linejoin', 'round').attr('stroke-width', 3);
+const gStatistics = g.append('g').attr('class', 'statistics');
 
 sendWebSocketRequest();
 setInterval(sendWebSocketRequest, 5000);
@@ -96,77 +70,78 @@ function update(data) {
 }
 
 function updateCropCircle(root) {
-  const t1 = d3.transition()
-    .duration(750);
+  const t1 = d3.transition().duration(750);
+  const t2 = d3.transition().delay(750).duration(750);
+  const t3 = d3.transition().delay(1500).duration(750);
+  const link = gLink.selectAll('path').data(root.links(), linkId);
 
-  const t2 = d3.transition()
-    .delay(750)
-    .duration(750);
-
-  const t3 = d3.transition()
-    .delay(1500)
-    .duration(750);
-
-  const link = gLink.selectAll('path')
-    .data(root.links(), linkId);
-
-  const linkEnter = link.enter().append('path')
-    .attr('id', d => linkId)
-    .attr('class', d => 'link ' + d.source.data.type)
+  const linkEnter = link
+    .enter()
+    .append('path')
+    .attr('id', (d) => linkId)
+    .attr('class', (d) => 'link ' + d.source.data.type)
     .style('opacity', 0.000001)
-    .attr('d', d3.linkRadial()
-                .angle(d => d.x)
-                .radius(d => d.y));
+    .attr(
+      'd',
+      d3
+        .linkRadial()
+        .angle((d) => d.x)
+        .radius((d) => d.y)
+    );
 
-  link.transition(t2)
+  link
+    .transition(t2)
     .style('opacity', 1.0)
-    .attr('d', d3.linkRadial()
-    .angle(d => d.x)
-    .radius(d => d.y));
+    .attr(
+      'd',
+      d3
+        .linkRadial()
+        .angle((d) => d.x)
+        .radius((d) => d.y)
+    );
 
-  linkEnter.transition(t3)
-    .style('opacity', 1.0);
+  linkEnter.transition(t3).style('opacity', 1.0);
+  link.exit().transition(t1).style('opacity', 0.000001).remove();
 
-  link.exit()
-    .transition(t1)
-    .style('opacity', 0.000001)
-    .remove();
+  const node = gNode.selectAll('g').data(root.descendants(), nodeId);
 
-  const node = gNode.selectAll('g')
-    .data(root.descendants(), nodeId);
-
-  const nodeEnter = node.enter().append('g')
+  const nodeEnter = node
+    .enter()
+    .append('g')
     .attr('id', nodeId)
-    .attr('class', d => 'node ' + d.data.type)
-    .attr('transform', d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`)
-    .on('mouseover', function() {
+    .attr('class', (d) => 'node ' + d.data.type)
+    .attr('transform', (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`)
+    .on('mouseover', function () {
       d3.select(this).select('text').style('font-size', 24).style('fill', '#FFF');
     })
-    .on('mouseout', function(d) {
+    .on('mouseout', function (d) {
       d3.select(this).select('text').style('font-size', 12).style('fill', '#999');
     });
 
-  nodeEnter.append('circle')
-    .attr('class', d => d.data.type)
+  nodeEnter
+    .append('circle')
+    .attr('class', (d) => d.data.type)
     .attr('fill', circleColor)
     .attr('r', circleRadius)
     .attr('cursor', 'pointer')
     .on('click', clickCircle)
     .style('opacity', 0.000001);
 
-  nodeEnter.append('text')
+  nodeEnter
+    .append('text')
     .attr('dy', '0.31em')
     .attr('x', labelOffsetX)
-    .attr('text-anchor', d => d.x < Math.PI === !d.children ? 'start' : 'end')
-    .attr('transform', d => d.x >= Math.PI ? 'rotate(180)' : null)
+    .attr('text-anchor', (d) => (d.x < Math.PI === !d.children ? 'start' : 'end'))
+    .attr('transform', (d) => (d.x >= Math.PI ? 'rotate(180)' : null))
     .style('opacity', 0.000001)
-    .text(d => d.data.name);
+    .text((d) => d.data.name);
 
-  nodeEnter.filter(d => d.data.type.includes('member'))
+  nodeEnter
+    .filter((d) => d.data.type.includes('member'))
     .append('text')
     .attr('class', 'member')
     .attr('dy', '0.31em')
-    .attr('transform', d => d.x >= Math.PI ? 'rotate(180)' : null)
+    .attr('transform', (d) => (d.x >= Math.PI ? 'rotate(180)' : null))
     .attr('cursor', 'pointer')
     .attr('text-anchor', 'middle')
     .on('click', clickCircle)
@@ -175,95 +150,52 @@ function updateCropCircle(root) {
     .style('opacity', 0.000001)
     .text(memberNumber);
 
-  nodeEnter.append('title')
-    .text(d => d.data.type);
+  nodeEnter.append('title').text((d) => d.data.type);
 
-  node.transition(t2)
-    .attr('transform', d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`)
+  node
+    .transition(t2)
+    .attr('transform', (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`)
     .select('circle.entity')
-      .attr('r', circleRadius)
-      .style('fill', entityColor)
-      .style('opacity', 1.0);
+    .attr('r', circleRadius)
+    .style('fill', entityColor)
+    .style('opacity', 1.0);
 
-  node.transition(t2)
-    .select('circle.shard')
-      .attr('r', circleRadius)
-      .style('fill', shardColor)
-      .style('opacity', 1.0);
-
-  node.transition(t2)
-    .select('circle.member')
-      .attr('r', circleRadius)
-      .style('fill', circleColor)
-      .style('opacity', 1.0);
-
-  node.transition(t2)
-    .select('text')
-      .style('opacity', 1.0);
-
-  node.transition(t2)
-    .select('text.member')
-      .style('opacity', 1.0);
-
-  nodeEnter.transition(t3)
-    .select('circle')
-      .style('opacity', 1.0);
-
-  nodeEnter.transition(t3)
-    .select('text')
-      .style('opacity', 1.0);
-
-  nodeEnter.transition(t3)
-    .select('text.member')
-      .style('opacity', 1.0);
-
-  node.exit()
-    .transition(t1)
-    .select('circle')
-      .attr('r', circleRadiusExit)
-      .style('opacity', 0.000001)
-      .style('fill', 'red');
-
-  node.exit()
-    .transition(t1)
-    .select('text')
-      .style('opacity', 0.000001);
-
-  node.exit()
-    .transition(t1)
-    .select('text.member')
-      .style('opacity', 0.000001);
-
-  node.exit()
-    .transition(t1)
-    .remove();
+  node.transition(t2).select('circle.shard').attr('r', circleRadius).style('fill', shardColor).style('opacity', 1.0);
+  node.transition(t2).select('circle.member').attr('r', circleRadius).style('fill', circleColor).style('opacity', 1.0);
+  node.transition(t2).select('text').style('opacity', 1.0);
+  node.transition(t2).select('text.member').style('opacity', 1.0);
+  nodeEnter.transition(t3).select('circle').style('opacity', 1.0);
+  nodeEnter.transition(t3).select('text').style('opacity', 1.0);
+  nodeEnter.transition(t3).select('text.member').style('opacity', 1.0);
+  node.exit().transition(t1).select('circle').attr('r', circleRadiusExit).style('opacity', 0.000001).style('fill', 'red');
+  node.exit().transition(t1).select('text').style('opacity', 0.000001);
+  node.exit().transition(t1).select('text.member').style('opacity', 0.000001);
+  node.exit().transition(t1).remove();
 }
 
 function updateClusterView(hierarchy) {
   const side = Math.min(width, height) / 20;
-  const members = gMembers.selectAll('g')
-    .data(memberData());
+  const members = gMembers.selectAll('g').data(memberData());
 
-  const membersEnter = members.enter().append('g')
-    .attr('cursor', 'pointer')
-    .on('click', clickMember);
+  const membersEnter = members.enter().append('g').attr('cursor', 'pointer').on('click', clickMember);
 
-  membersEnter.append('rect')
-    .attr('x', d => d.x)
-    .attr('y', d => d.y)
+  membersEnter
+    .append('rect')
+    .attr('x', (d) => d.x)
+    .attr('y', (d) => d.y)
     .attr('width', side)
     .attr('height', side)
-    .style('fill', d => d.active ? '#30d35a' : '#555');
+    .style('fill', (d) => (d.active ? '#30d35a' : '#555'));
 
-  membersEnter.append('text')
-    .attr('x', d => d.x + side / 5)
-    .attr('y', d => d.y + side / 2)
+  membersEnter
+    .append('text')
+    .attr('x', (d) => d.x + side / 5)
+    .attr('y', (d) => d.y + side / 2)
     .style('font-size', 24)
     .style('fill', '#FFF')
-    .text(d => d.memberId - 2550);
+    .text((d) => d.memberId - 2550);
 
-  members.select('rect')
-    .style('fill', d => d.active ? '#30d35a' : '#555');
+  members.select('rect').style('fill', (d) => (d.active ? '#30d35a' : '#555'));
 
   members.select('text');
 
@@ -271,7 +203,7 @@ function updateClusterView(hierarchy) {
     const members = [];
     let memberId = 2551;
     for (var row = 0; row < 3; row++) {
-      for (var col  = 0; col < 3; col++) {
+      for (var col = 0; col < 3; col++) {
         const x = col * (side + 2) + side / 2 - width / 2;
         const y = row * (side + 2) + side / 2 - height / 2;
         members.push({ memberId: memberId, x: x, y: y, active: isActive(memberId), address: address(memberId) });
@@ -282,65 +214,60 @@ function updateClusterView(hierarchy) {
   }
 
   function address(m) {
-    const idx = hierarchy.children
-      ? hierarchy.children.findIndex(d => d.name.endsWith(m))
-      : -1;
+    const idx = hierarchy.children ? hierarchy.children.findIndex((d) => d.name.endsWith(m)) : -1;
     return idx >= 0 ? hierarchy.children[idx].name : '';
   }
 
   function isActive(m) {
-    return hierarchy.children
-      ? hierarchy.children.findIndex(d => d.name.endsWith(m)) >= 0
-      : false;
+    return hierarchy.children ? hierarchy.children.findIndex((d) => d.name.endsWith(m)) >= 0 : false;
   }
 }
 
 function updateServerLinks(data, shardingLinks) {
   const links = serversLinks(data, shardingLinks);
 
-  const t1 = d3.transition()
-    .duration(750);
+  const t1 = d3.transition().duration(750);
+  const t2 = d3.transition().delay(750).duration(750);
+  const t3 = d3.transition().delay(1500).duration(750);
+  const link = gHttpServerLink.selectAll('path.http-server').data(links, (d) => d.source.id + '-' + d.target.id);
 
-  const t2 = d3.transition()
-    .delay(750)
-    .duration(750);
-
-  const t3 = d3.transition()
-    .delay(1500)
-    .duration(750);
-
-  const link = gHttpServerLink.selectAll('path.http-server')
-    .data(links, d => d.source.id + '-' + d.target.id);
-
-  const linkEnter = link.enter().append('path')
+  const linkEnter = link
+    .enter()
+    .append('path')
     .attr('id', function (d) {
-                      return d.source.id + '-' + d.target.id; })
-    .attr('class', d => 'http-server http-server-id-' + d.source.id)
-    .attr('stroke', d => d3.schemeSet3[Number(d.source.id) % d3.schemeSet3.length])
+      return d.source.id + '-' + d.target.id;
+    })
+    .attr('class', (d) => 'http-server http-server-id-' + d.source.id)
+    .attr('stroke', (d) => d3.schemeSet3[Number(d.source.id) % d3.schemeSet3.length])
     .style('opacity', 0.000001)
-    .attr('d', d3.linkRadial()
-                .angle(d => d.x)
-                .radius(d => d.y));
+    .attr(
+      'd',
+      d3
+        .linkRadial()
+        .angle((d) => d.x)
+        .radius((d) => d.y)
+    );
 
-  link.transition(t2)
+  link
+    .transition(t2)
     .style('opacity', 1.0)
-    .attr('d', d3.linkRadial()
-                .angle(d => d.x)
-                .radius(d => d.y));
+    .attr(
+      'd',
+      d3
+        .linkRadial()
+        .angle((d) => d.x)
+        .radius((d) => d.y)
+    );
 
-  linkEnter.transition(t3)
-    .style('opacity', 1.0);
+  linkEnter.transition(t3).style('opacity', 1.0);
 
-  link.exit()
-    .transition(t1)
-    .style('opacity', 0.000001)
-    .remove();
+  link.exit().transition(t1).style('opacity', 0.000001).remove();
 
   function serversLinks(data, shardingLinks) {
     const links = [];
-    data.forEach(s => {
+    data.forEach((s) => {
       const server = s.server;
-      const serverLink = shardingLinks.find(l => l.target.data.name.includes(server));
+      const serverLink = shardingLinks.find((l) => l.target.data.name.includes(server));
       if (serverLink && showServerLinks(server)) {
         links.push(...serverLinks(server, serverLink.target, s.links, shardingLinks));
       }
@@ -350,14 +277,14 @@ function updateServerLinks(data, shardingLinks) {
 
   function serverLinks(server, source, serverLinks, shardingLinks) {
     const links = [];
-    serverLinks.forEach(l => {
+    serverLinks.forEach((l) => {
       const entityId = l.entityId;
-      const entityLink = shardingLinks.find(l => l.target.data.name == entityId);
+      const entityLink = shardingLinks.find((l) => l.target.data.name == entityId);
       if (entityLink) {
         const sourceId = server.split(':')[2];
         links.push({
           source: { id: sourceId, x: source.x, y: source.y },
-          target: { id: entityId, x: entityLink.target.x, y: entityLink.target.y }
+          target: { id: entityId, x: entityLink.target.x, y: entityLink.target.y },
         });
       }
     });
@@ -390,53 +317,52 @@ function updateStatistics(data, shardingDataLinks) {
     labelsValues.push({ x: x, y: y + 2 * (grid + margin), label: 'Message rate', value: messageRatePerSecond.toLocaleString() + '/s' });
   }
 
-  const nodes = gStatistics.selectAll('g')
-    .data(labelsValues);
+  const nodes = gStatistics.selectAll('g').data(labelsValues);
 
-  const nodesEnter = nodes.enter().append('g')
-    .attr('cursor', 'pointer')
-    .on('click', clickMember);
+  const nodesEnter = nodes.enter().append('g').attr('cursor', 'pointer').on('click', clickMember);
 
-  nodesEnter.append('rect')
-    .attr('x', d => d.x)
-    .attr('y', d => d.y)
+  nodesEnter
+    .append('rect')
+    .attr('x', (d) => d.x)
+    .attr('y', (d) => d.y)
     .attr('width', widthLabel)
     .attr('height', grid)
     .style('fill', bgColor);
 
-  nodesEnter.append('text')
-    .attr('x', d => d.x + margin * 2)
-    .attr('y', d => d.y + grid - margin * 2)
+  nodesEnter
+    .append('text')
+    .attr('x', (d) => d.x + margin * 2)
+    .attr('y', (d) => d.y + grid - margin * 2)
     .attr('text-anchor', 'start')
     .style('font-size', grid - margin * 2.5)
     .style('fill', txColor)
-    .text(d => d.label);
+    .text((d) => d.label);
 
-  nodesEnter.append('rect')
-    .attr('x', d => d.x + widthLabel + margin)
-    .attr('y', d => d.y)
+  nodesEnter
+    .append('rect')
+    .attr('x', (d) => d.x + widthLabel + margin)
+    .attr('y', (d) => d.y)
     .attr('width', widthValue)
     .attr('height', grid)
-    .style('fill', d => d.y == y ? bgColorEntityCount : bgColor);
+    .style('fill', (d) => (d.y == y ? bgColorEntityCount : bgColor));
 
-  nodesEnter.append('text')
-    .attr('x', d => d.x + widthLabel + widthValue - margin * 2)
-    .attr('y', d => d.y + grid - margin * 2)
+  nodesEnter
+    .append('text')
+    .attr('x', (d) => d.x + widthLabel + widthValue - margin * 2)
+    .attr('y', (d) => d.y + grid - margin * 2)
     .attr('text-anchor', 'end')
     .attr('class', 'statistics')
     .style('font-size', grid - margin * 2.5)
     .style('fill', txColor)
-    .text(d => d.value);
+    .text((d) => d.value);
 
-  nodes.select('text.statistics')
-    .text(d => d.value);
+  nodes.select('text.statistics').text((d) => d.value);
 
-  nodes.exit()
-    .remove();
+  nodes.exit().remove();
 }
 
 function showServerLinks(server) {
-  return hiddenMemberLinkViews.find(s => s.includes(server)) ? false : true;
+  return hiddenMemberLinkViews.find((s) => s.includes(server)) ? false : true;
 }
 
 function linkId(d) {
@@ -554,10 +480,18 @@ d3.select('body').on('keydown', function () {
 });
 
 const svgElement = document.querySelector('svg');
-svgElement.onfocus = function () { svgFocus(true); };
-svgElement.onblur = function () { svgFocus(false); };
-svgElement.onmouseenter = function () { svgFocus(true); };
-svgElement.onmouseleave = function () { svgFocus(false); };
+svgElement.onfocus = function () {
+  svgFocus(true);
+};
+svgElement.onblur = function () {
+  svgFocus(false);
+};
+svgElement.onmouseenter = function () {
+  svgFocus(true);
+};
+svgElement.onmouseleave = function () {
+  svgFocus(false);
+};
 
 function svgFocus(hasFocus) {
   svgHasFocus = hasFocus;
